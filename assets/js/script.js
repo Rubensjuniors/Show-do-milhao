26d4c34958572584a1e1256c75ff966bc0c06631
Rubens.junior <rubensjunio2110@gmail.com>
2023-03-04 14:54:04 -0300

    refact: do arquivo  script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,70 @@
-const bord = document.querySelector('[data-questions="board"]');
+const boardContainer = document.querySelector('[data-questions="board"]');
 
-const a = document.querySelector('[data-questions="a"]');
-const b = document.querySelector('[data-questions="b"]');
-const c = document.querySelector('[data-questions="c"]');
-const d = document.querySelector('[data-questions="d"]');
+const alternativeAText = document.querySelector('[data-questions="alternative-a"]');
+const alternativeBText = document.querySelector('[data-questions="alternative-b"]');
+const alternativeCText = document.querySelector('[data-questions="alternative-c"]');
+const alternativeDText = document.querySelector('[data-questions="alternative-d"]');
 
-const buttonStart = document.querySelector('.start__button--js'); 
-const buttonsTema = document.querySelectorAll('.choose__button--js');
-const containers = document.querySelectorAll('[data-visible=""]');
+const containerButtons = document.querySelectorAll('[data-visible=""]');
 
+function handleContainerButtonClick(event) {
+  const hiddenClass = "hiden";
+  const buttons = event.target.getAttribute("data-button");
 
-containers.forEach(container => {
-  console.log(container)
-})
-
-function getData(questions) {
-  fetch('./assets/json/data.json')
-  .then(response =>{
-        return response.json();
-  })
-  .then(data =>{
-    setData(data, questions)
-    
-  })     
-}
-
-function setData(data, questions) {
-  if(questions >= data.questions.length) {
-    questions = 1
+  if (!this.classList.contains(hiddenClass)) {
+    if (typeof buttons === "string") {
+      this.classList.add(hiddenClass);
+      containerButtons[1].classList.remove(hiddenClass);
+      if (buttons === "variado") {
+        this.classList.add(hiddenClass);
+        containerButtons[2].classList.remove(hiddenClass);
+        fetchQuestions(Math.floor(Math.random() * 30), "./assets/json/data.json");
+      }else if(buttons === "portugue"){
+        console.log('off')
+      }else{
+        console.log('off')
+      }
+    }
   }
-  
-  bord.textContent = data.questions[questions].pergunta;
-
-  a.textContent= data.questions[questions].alternativaA;
-  b.textContent= data.questions[questions].alternativaB;
-  c.textContent= data.questions[questions].alternativaC;
-  d.textContent= data.questions[questions].alternativaD;
-
-  const {correta} = data.questions[questions]
 }
 
- getData(Math.floor(Math.random() * 31));
+containerButtons.forEach((button) => {
+  button.addEventListener("click", handleContainerButtonClick);
+});
+
+function fetchQuestions(questionIndex, url) {
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      setQuestions(data.questions[questionIndex]);
+    })
+    .catch((error) => {
+      console.error("Error fetching questions:", error);
+    });
+}
+
+function setQuestions(questionData) {
+  const correctAnswer = questionData.correta;
+
+  boardContainer.textContent = questionData.pergunta;
+
+  alternativeAText.textContent = questionData.alternativaA;
+  alternativeBText.textContent = questionData.alternativaB;
+  alternativeCText.textContent = questionData.alternativaC;
+  alternativeDText.textContent = questionData.alternativaD;
+
+ 
+}
+
+
+
+
+
+
 
 
