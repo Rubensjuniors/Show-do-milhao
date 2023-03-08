3b643da139d5b7a029565dfbee61e07741f4b729
rubensjuniors <rubensjunio2110@gmail.com>
2023-03-08 17:21:53 -0300

    feat: adicionado verificação de resposta

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,31 @@
-const boardContainer = document.querySelector('[data-questions="board"]');
 
-const alternativeAText = document.querySelector('[data-questions="alternative-a"]');
-const alternativeBText = document.querySelector('[data-questions="alternative-b"]');
-const alternativeCText = document.querySelector('[data-questions="alternative-c"]');
-const alternativeDText = document.querySelector('[data-questions="alternative-d"]');
+const containerButtons = document.querySelectorAll(".visible");
 
-const containerButtons = document.querySelectorAll('[data-visible=""]');
+const questionsCounter = document.querySelector('#questions__counter--js');
+const panelCoin = document.querySelector('#questions__coin-js');
+const panelTime = document.querySelector('#questions__time-js');
 
- const urlQuestionsVariado = "./assets/json/variado.json";
- const urlQuestionsPortugues = "./assets/json/portugues.json";
- const urlQuestionsMatematica= "./assets/json/matematica.json";
- const hiddenClass = "hiden";
+const boardContainer = document.querySelector('#questions__board-js');
 
+const helpsQuestions = document.querySelectorAll(".help__button--js");
 
+const alternativasButtons = document.querySelectorAll('.response__options-js');
+const resposta = document.querySelectorAll('[data-response]');
 
 
+const alternativeAText = document.querySelector("#alternative-a");
+const alternativeBText = document.querySelector("#alternative-b");
+const alternativeCText = document.querySelector("#alternative-c");
+const alternativeDText = document.querySelector("#alternative-d");
 
- function handleContainerButtonClick(event) {
+
+const hiddenClass = "hiden";
+const urlQuestionsVariado = "./assets/json/variado.json";
+const urlQuestionsPortugues = "./assets/json/portugues.json";
+const urlQuestionsMatematica = "./assets/json/matematica.json";
+let json = null
+
+function handleContainerButtonClick(event) {
   const buttons = event.target.getAttribute("data-button");
   if (this.classList.contains(hiddenClass) || !buttons) {
     return;
@@ -45,6 +54,7 @@ const containerButtons = document.querySelectorAll('[data-visible=""]');
 }
 containerButtons.forEach((button) => {
   button.addEventListener("click", handleContainerButtonClick);
+  button.addEventListener("touchstart", handleContainerButtonClick);
 });
 
 function fetchQuestions(questionIndex, url) {
@@ -57,6 +67,8 @@ function fetchQuestions(questionIndex, url) {
     })
     .then((data) => {
       setQuestions(data.questions[questionIndex]);
+      json = data.questions[questionIndex]
+      console.log(json.correta)
     })
     .catch((error) => {
       console.error("Error fetching questions:", error);
@@ -64,18 +76,28 @@ function fetchQuestions(questionIndex, url) {
 }
 
 function setQuestions(questionData) {
-  const correctAnswer = questionData.correta;
-
   boardContainer.textContent = questionData.pergunta;
-
   alternativeAText.textContent = questionData.alternativaA;
   alternativeBText.textContent = questionData.alternativaB;
   alternativeCText.textContent = questionData.alternativaC;
   alternativeDText.textContent = questionData.alternativaD;
+}
+
+function handleClickAlternativa(){
+  const {correta} = json
+  const picked = this.querySelector('div').innerText;
+
+  if(correta === picked){
+    console.log("deu")
+  }else{
+    console.log('nao')
+  }
 
  
 }
+alternativasButtons.forEach(alternativa => {
+alternativa.addEventListener('click', handleClickAlternativa)
+alternativa.addEventListener('touchstart', handleClickAlternativa)
+})
 
-
-fetchQuestions(Math.floor(Math.random() * 30), urlQuestionsVariado);
 
